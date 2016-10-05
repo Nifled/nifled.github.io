aa432e8b84f17acfb5019c7e14036ced38c985ef
Erick <e_delfin_23@yahoo.com>
2016-10-04 21:24:34 -0700

    Add website files :ok_hand:

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// blurred image background !!!!!!!!
+
+
+
+// Alternative color for div backgrounds: light blue #A7FFEB, material green: #64FFDA
+
+//  http://www.wideeyecreative.com/
+
+
+//  FOOTER GO UPPP!!!
+
+
+
+
+
+//  TODO:
+//  https://github.com/matthieua/WOW
+//  https://github.com/matthieua/WOW
+//  http://www.webalys.com/
+
+
+//***  Navbar hamburger (made it with animation)
+//  Make and place a FOOTER TO FORGET ABOUT THE FORM SHENANIGANS
+//  Finish up the project section, maybe a hover function, tweak up the bootsrap grids
+//  Fix text sizing in blockquote
+//  Fix pic sizing in SKILLS (looks to big when scaled down)
+//  when resizing, navbar goes over the banner page & pic
+//  For the projects, use images 1080 x 720, looks better
