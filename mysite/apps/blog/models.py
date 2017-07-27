e934363f712bc118ef22b688f6f4aa67993b5e2a
Erick Delfin <erickdelfinmolina@gmail.com>
2017-07-26 23:59:15 -0700

    Modified field attrs in Post

--- mysite/apps/blog/models.py
+++ mysite/apps/blog/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from django.utils.safestring import mark_safe
+from markdown_deux import markdown
 
 
 class Post(models.Model):
-    slug = models.SlugField(db_index=True, unique=True, max_length=255)
+    slug = models.SlugField(db_index=True, unique=True, max_length=255, editable=False)  # Hide from admin
     title = models.TextField(db_index=True, max_length=255,)
     body = models.TextField()
-    read_time = models.PositiveSmallIntegerField()
+    read_time = models.PositiveSmallIntegerField(editable=False)  # Hide from admin
 
     tags = models.ManyToManyField(
-        'blog.Tag', related_name='posts'
+        'blog.Tag', related_name='posts', blank=True
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def get_markdown(self):
+        content = self.body
+        markdown_text = markdown(content)
+        return mark_safe(markdown_text)
 
     def __str__(self):
         return self.title
