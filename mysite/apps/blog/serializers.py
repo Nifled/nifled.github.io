e19e0bc9ac48ba7eae82ec4ecdf168a0aa359a1a
Erick Delfin <erickdelfinmolina@gmail.com>
2017-07-30 00:33:46 -0700

    Add Post serializer

---
+++ mysite/apps/blog/serializers.py
@@ -0,0 +1,49 @@
+from rest_framework import serializers
+
+from .models import Post, Tag
+
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+
+    slug = serializers.SlugField(required=False)
+    description = serializers.CharField(required=False)
+
+    # Show the list of tags' __str__ string in our JSON
+    # response. So we can retrieve them as an array.
+    tagList = serializers.StringRelatedField(many=True, required=False, source='tags')
+
+    created_at = serializers.SerializerMethodField(method_name='get_created_at')
+    updated_at = serializers.SerializerMethodField(method_name='get_updated_at')
+
+    class Meta:
+        model = Post
+        fields = (
+            'body',
+            'description',
+            'read_time',
+            'slug',
+            'tagList',
+            'title',
+            'url',
+        )
+
+    def create(self, validated_data):
+        """Create tags dynamically along with other validated data."""
+
+        tags = validated_data.pop('tags', [])
+
+        post = Post.objects.create(**validated_data)
+
+        for tag in tags:
+            post.tags.add(tag)
+
+        return post
+
+    # \
+    #  Serializer methods
+    # /
+    def get_created_at(self, instance):
+        return instance.created_at.isoformat()
+
+    def get_updated_at(self, instance):
+        return instance.updated_at.isoformat()
